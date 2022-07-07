ab552683b28acc453abe1c9c85836e7360bd7109
Derngineer <77061345+Derngineer@users.noreply.github.com>
2022-07-07 21:38:08 +0400

    Add files via upload
    This is a solution to a palindrome detector, it picks if l word, passage, or paragraph is a palindrome by converting str to list, reversing it and comparing. It might be a long way around the problem but it definitely fixes it.
    
    Nb the code removes punctuations , spaces and takes everything in lower case form

---
+++ challenge_work.py
@@ -0,0 +1,56 @@
+
+# def Punctuation(string):
+
+# punctuations = '''!()-[]{}; :'"\,<>./?@#$%^&*_~'''
+
+# for x in string.lower():
+
+#     if x in punctuations:
+
+#         string = string.replace(x,"")
+
+# print(string)
+
+
+from string import punctuation
+
+
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''" "
+
+work = input("Enter a word or a sentence")
+
+no_punct =""
+
+for char in work:
+
+    if char not in punctuation:
+
+        no_punct = no_punct + char
+
+
+new = no_punct.lower()
+
+new_2 = new.replace(' ','')
+
+print(new_2)  
+
+
+
+list_1 = list(new_2.strip(""))
+
+
+
+list_2 = list_1[::-1]
+
+
+print(list_1)
+
+print(list_2)
+
+if list_1 == list_2:
+
+    print("True")
+
+else:
+    print ('false')       
+
